8d4ef09a6c545810cf4075edde784bbc17a3b523
Isaac Byrne <isaacbyrne@Isaacs-Air.PK5001Z>
2015-11-20 15:01:24 -0700

    added php modules

---
+++ modules/2x2likedphotos.php
@@ -0,0 +1,24 @@
+<div id="social-module-item-right">
+		<table width="100%" border="0" cellspacing="0" cellpadding="0" id="social-module-title">
+			<tbody>
+				<tr>
+					<td width="50%"><div id="blog-column-header-line">&nbsp;</div></td>
+					<td><span class="blog-column-header-title" >COMMUNITY</span></td>
+					<td width="50%"><div id="blog-column-header-line">&nbsp;</div></td>
+				</tr>
+			</tbody>
+		</table>
+		<div id="instafeed2" style="width:273px; margin-top: 20px;">
+			<!-- Instagram Pics Here -->
+		</div>
+		<script type="text/javascript">
+			var feed2 = new Instafeed({
+				target: 'instafeed2',
+				get: 'liked',
+				limit: 4,
+				accessToken: '806978688.467ede5.f077f17b8f7a47c8a6914316b0822f62', //Replace with your access token
+				template: '<img style="width:129px; margin-left:3px;" src="{{image}}"/>'
+			});
+			feed2.run();
+		</script>
+</div>
---
+++ modules/2x2userphotos.php
@@ -0,0 +1,26 @@
+<div id="social-module-item-left">
+		<table width="100%" border="0" cellspacing="0" cellpadding="0" id="social-module-title">
+			<tbody>
+				<tr>
+					<td width="50%"><div id="blog-column-header-line">&nbsp;</div></td>
+					<td><a class="blog-column-header-title" href="http://www.instagram.com/alkalinetea" target="_blank"><span>INSTAGRAM</span></a></td>
+					<td width="50%"><div id="blog-column-header-line">&nbsp;</div></td>
+				</tr>
+			</tbody>
+		</table>
+		<div id="instafeed" style="width:273px; margin-top: 20px;">
+			<!-- Instagram Pics Here -->
+		</div>
+		<script type="text/javascript">
+					var feed = new Instafeed({
+						target: 'instafeed',
+						get: 'user',
+						userId: 806978688,
+						limit: 4,
+						sortBy: 'most-recent',
+						accessToken: '806978688.467ede5.f077f17b8f7a47c8a6914316b0822f62', //Replace with your access token
+						template: '<img style="width:129px; margin-left:3px;" src="{{image}}"/>'
+					});
+					feed.run();
+		</script>
+</div>
